2e42dc0546a5ba8c88c7e0a4864492018d2453a5
Rahul G <rahul.g8270@gmail.com>
2024-11-09 23:44:47 +0530

    countries UI implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {  useState,useEffect } from "react";
+import axios from 'axios'
+import Card from "./Components/Card";
+import "./App.css"
 
 function App() {
+  const API = `https://xcountries-backend.azurewebsites.net/all`;
+  const [countries,setCountries] = useState([])
+
+  const fetchCountries = ()=>{
+    axios.get(API)
+     .then((country) => {
+        setCountries(country.data)
+     })
+     .catch(error => console.error("Error fetching data: " +error))
+  }
+
+  useEffect(()=>{
+    fetchCountries()
+  },[countries])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cards-container">
+      {countries.length > 0 ? ( countries.map((country,index) => {
+        console.log(country.name)
+        return <Card item={country} key={index}/>
+      })) : <h1>loading...</h1>}
+      </div>
     </div>
   );
 }
---
+++ src/Components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Card.css'
+const Card = ({item,index}) => {
+  return (
+    <div className="cardContainer" key={index}>
+        <div className='cardImage'>
+            <img src={item.flag} alt="" />
+        </div>
+        <h1 className='countryName'>{item.name}</h1>
+    </div>
+  )
+}
+
+export default Card
