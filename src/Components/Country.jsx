d3cd00b5b8203c96a8b1ceb7d243748b4d63ee44
Rahul G <rahul.g8270@gmail.com>
2024-11-10 00:55:18 +0530

    made the alt tag to display country name

--- src/Components/Country.jsx
+++ src/Components/Country.jsx
@@ -4,7 +4,7 @@ const Country = ({item,index}) => {
   return (
     <div className="countryContainer" key={index}>
         <div className='countryImage'>
-            <img src={item.flag} alt={item.name} />
+            <img src={item.flag} alt={`Flag of ${item.name}`} />
         </div>
         <h1 className='countryName'>{item.name}</h1>
     </div>
